c77dc46d8c50b09ae17b89b8982f876136af3958
phoenix <phoenixMayday0@gmail.com>
2025-02-25 15:33:25 +0000

    Update load testing setup bash file to new method

--- load_test_setup.sh
+++ load_test_setup.sh
@@ -1,37 +1,25 @@
-# 1. Create Network Namespaces
+#!/bin/bash
+
+# 1. Create network namespaces
 sudo ip netns add ns1
 sudo ip netns add ns2
-sudo ip netns add router
 
 # 2. Create virtual ethernet (veth) pairs
-sudo ip link add veth0 type veth peer name veth0-rtr
-sudo ip link add veth1 type veth peer name veth1-rtr
+sudo ip link add veth0 type veth peer name veth1
 
 # 3. Assign the veths to namespaces
 sudo ip link set veth0 netns ns1
 sudo ip link set veth1 netns ns2
 
-sudo ip link set veth0-rtr netns router
-sudo ip link set veth1-rtr netns router
-
 # 4. Assign IPs and bring up interfaces
 sudo ip netns exec ns1 ip addr add 192.168.1.1/24 dev veth0
-sudo ip netns exec ns2 ip addr add 192.168.2.1/24 dev veth1
-sudo ip netns exec router ip addr add 192.168.1.100/24 dev veth0-rtr
-sudo ip netns exec router ip addr add 192.168.2.100/24 dev veth1-rtr
+sudo ip netns exec ns2 ip addr add 192.168.1.2/24 dev veth1
 
 sudo ip netns exec ns1 ip link set veth0 up
 sudo ip netns exec ns2 ip link set veth1 up
-sudo ip netns exec router ip link set veth0-rtr up
-sudo ip netns exec router ip link set veth1-rtr up
 
 sudo ip netns exec ns1 ip link set lo up
 sudo ip netns exec ns2 ip link set lo up
 
-# 5. Update default routes in namespaces
-sudo ip netns exec ns1 ip route add default via 192.168.1.100 dev veth0
-#sudo ip netns exec ns2 ip route add default via 192.168.2.100 dev veth1 #not needed maybe?
-
-# 6. Set up forwarding rules
-sudo ip netns exec router iptables -A FORWARD -i veth0-rtr -o veth1-rtr -j NFQUEUE --queue-num 0
-sudo ip netns exec router iptables -A FORWARD -i veth1-rtr -o veth0-rtr -m state --state ESTABLISHED,RELATED -j ACCEPT #NFQUEUE --queue-num 0
+# 5. Set up input rule
+sudo ip netns exec ns2 iptables -A INPUT -j NFQUEUE --queue-num 0 

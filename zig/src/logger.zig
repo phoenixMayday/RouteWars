234f41afe2bb4c0d56815eb67449acb025b55ac9
phoenix <phoenixMayday0@gmail.com>
2025-03-12 21:08:12 +0000

    Begin adding zig build file and different I/O logging modes

---
+++ zig/src/logger.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+// import log mode build option
+const log_mode = @import("build_options").log_mode;
+
+const IOMode = enum {
+    NoIO,
+    IOPrints,
+    IOLogging,
+};
+
+const current_mode: IOMode = blk: {
+    if (std.mem.eql(u8, log_mode, "no-io")) {
+        break :blk IOMode.NoIO;
+    } else if (std.mem.eql(u8, log_mode, "io-prints")) {
+        break :blk IOMode.IOPrints;
+    } else if (std.mem.eql(u8, log_mode, "io-logging")) {
+        break :blk IOMode.IOLogging;
+    } else {
+        @compileError("Invalid log mode. Use 'no-io', 'io-prints', or 'io-logging'.");
+    }
+};
+
+pub fn log(comptime fmt: []const u8, args: anytype) void {
+    switch (current_mode) {
+        .NoIO => {
+            // no I/O operations
+        },
+        .IOPrints => {
+            // print to stdout
+            std.debug.print(fmt, args);
+        },
+        .IOLogging => {
+            // log to a file
+            const file = std.fs.cwd().createFile("log.txt", .{ .append = true }) catch |err| {
+                std.debug.print("Failed to open log file: {}\n", .{err});
+                return;
+            };
+            defer file.close();
+
+            file.writer().print(fmt, args) catch |err| {
+                std.debug.print("Failed to write to log file: {}\n", .{err});
+            };
+        },
+    }
+}
--- zig/src/nfq_accept_all_qname.zig
+++ zig/src/nfq_accept_all_qname.zig
@@ -9,6 +9,7 @@ const netinet = @cImport({
     @cInclude("netinet/udp.h");
 });
 const dns = @import("dns.zig");
+const logger = @import("logger.zig");
 
 const QueueHandle = ?*netfilter.nfq_handle;
 const Queue = ?*netfilter.nfq_q_handle;
@@ -21,7 +22,7 @@ fn callback(queue: Queue, nfmsg: ?*netfilter.nfgenmsg, nfa: ?*netfilter.nfq_data
     const packet_hdr = netfilter.nfq_get_msg_packet_hdr(nfa);
 
     if (packet_hdr == null) {
-        std.debug.print("Error: Failed to get packet header\n", .{});
+        logger.log("Error: Failed to get packet header\n", .{});
         return netfilter.NF_DROP;
     }
     id = std.mem.bigToNative(u32, packet_hdr.*.packet_id);
@@ -32,7 +33,7 @@ fn callback(queue: Queue, nfmsg: ?*netfilter.nfgenmsg, nfa: ?*netfilter.nfq_data
     payload_len = netfilter.nfq_get_payload(nfa, &payload);
 
     if (payload_len < 0) {
-        std.debug.print("Error: Failed to get packet payload\n", .{});
+        logger.log("Error: Failed to get packet payload\n", .{});
         return netfilter.NF_DROP;
     }
 
@@ -48,16 +49,16 @@ fn callback(queue: Queue, nfmsg: ?*netfilter.nfgenmsg, nfa: ?*netfilter.nfq_data
         // Decode the QNAME
         var buffer: [256]u8 = undefined;
         const qname = dns.decodeQname(dns_payload[0..], &buffer) catch |err| {
-            std.debug.print("Error decoding QNAME: {}\n", .{err});
+            logger.log("Error decoding QNAME: {}\n", .{err});
             return netfilter.nfq_set_verdict(queue, id, netfilter.NF_ACCEPT, 0, null);
         };
 
-        std.debug.print("UDP DNS Packet handled (ID: {}), Domain: {s}\n", .{ id, qname });
+        logger.log("UDP DNS Packet handled (ID: {}), Domain: {s}\n", .{ id, qname });
     }
     // TCP DNS
     else if (ip_header.ip_p == netinet.IPPROTO_TCP) {
         // Most DNS traffic is UDP so we won't implement this for now.
-        std.debug.print("TCP Packet handled (ID: {})\n", .{id});
+        logger.log("TCP Packet handled (ID: {})\n", .{id});
     }
 
     // Accept all packets
@@ -73,34 +74,34 @@ pub fn main() !void {
 
     h = netfilter.nfq_open();
     if (h == null) {
-        std.debug.print("Error during nfq_open()\n", .{});
+        logger.log("Error during nfq_open()\n", .{});
         std.process.exit(1);
     }
     defer _ = netfilter.nfq_close(h);
 
     // Unbind existing nf_queue handler for AF_INET (if any)
     if (netfilter.nfq_unbind_pf(h, netfilter.AF_INET) < 0) {
-        std.debug.print("Error during nfq_unbind_pf()\n", .{});
+        logger.log("Error during nfq_unbind_pf()\n", .{});
         std.process.exit(1);
     }
 
     // Bind nfnetlink_queue as the handler for AF_INET
     if (netfilter.nfq_bind_pf(h, netfilter.AF_INET) < 0) {
-        std.debug.print("Error during nfq_bind_pf()\n", .{});
+        logger.log("Error during nfq_bind_pf()\n", .{});
         std.process.exit(1);
     }
 
     // Create a new queue handle
     qh = netfilter.nfq_create_queue(h, 0, callback, null);
     if (qh == null) {
-        std.debug.print("Error during nfq_create_queue()\n", .{});
+        logger.log("Error during nfq_create_queue()\n", .{});
         std.process.exit(1);
     }
     defer _ = netfilter.nfq_destroy_queue(qh);
 
     // Set the mode to copy packet data to userspace
     if (netfilter.nfq_set_mode(qh, netfilter.NFQNL_COPY_PACKET, 0xffff) < 0) {
-        std.debug.print("Error during nfq_set_mode()\n", .{});
+        logger.log("Error during nfq_set_mode()\n", .{});
         std.process.exit(1);
     }
 
@@ -114,7 +115,7 @@ pub fn main() !void {
             // Handle the packet
             _ = netfilter.nfq_handle_packet(h, &buf, rv);
         } else {
-            std.debug.print("Error during recv()\n", .{});
+            logger.log("Error during recv()\n", .{});
             break;
         }
     }
